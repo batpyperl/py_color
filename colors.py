1ec0b756db5cec07271d7ea0a7946b0121cfb6a0
batpyperl <48865122+batpyperl@users.noreply.github.com>
2019-03-24 19:06:06 +0100

    Ajout fichiers python

---
+++ colors.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Afficher de la couleur dans la console
+"""
+
+from colorama import init, Fore, Back, Style
+
+init()
+
+# definition couleur
+green   = Fore.GREEN + Style.BRIGHT
---
+++ printing_colors.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from colors import green
+
+def main():
+    print(green + 'ça s\'affiche bien en vert')
+
+
+if __name__ == '__main__':
+    main()
