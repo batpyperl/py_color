8834d08704a605fd88bfe0701eba5317e7f69b09
batpyperl <48865122+batpyperl@users.noreply.github.com>
2019-03-24 19:13:02 +0100

    reset des couleurs
    Effectuer un reset après la fin d'utilisation d'une couleur.

--- printing_colors.py
+++ printing_colors.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from colors import green, red
+from colors import green, red, reset
 
 def main():
-    print(green + 'ça s\'affiche bien en vert')
-    print(red + 'En rouge aussi !')
+    print(green + 'ça s\'affiche bien en vert' + reset)
+    print(red + 'En rouge aussi !' + reset)
+    print('ah ben ouais ça change tout !!')
 
 
 if __name__ == '__main__':
