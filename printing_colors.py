ca373b9190069df99642826a9a71f679806c78d9
batpyperl <48865122+batpyperl@users.noreply.github.com>
2019-03-24 19:09:07 +0100

    utilisation couleur rouge
    Import de la couleur rouge dans le script principal

--- printing_colors.py
+++ printing_colors.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from colors import green
+from colors import green, red
 
 def main():
     print(green + 'ça s\'affiche bien en vert')
+    print(red + 'En rouge aussi !')
 
 
 if __name__ == '__main__':
